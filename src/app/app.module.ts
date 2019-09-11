849d4475c14dc6627ecc43942e41f335c651da16
Paul Whipp <pwhipp@csumb.edu>
2019-09-11 10:43:18 -0700

    generating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ThanksComponent } from './thanks/thanks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProductlistComponent,
+    ItemdetailsComponent,
+    CheckoutComponent,
+    ThanksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/itemdetails/itemdetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-itemdetails',
+  templateUrl: './itemdetails.component.html',
+  styleUrls: ['./itemdetails.component.scss']
+})
+export class ItemdetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
