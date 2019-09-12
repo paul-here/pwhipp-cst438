fa9a1a7b22e18ddcbf76c88e014dcb67cb81cf04
Paul Whipp <pwhipp@csumb.edu>
2019-09-11 17:01:36 -0700

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Component Imports
+import { HomeComponent } from './home/home.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ItemdetailsComponent } from './itemdetails/itemdetails.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ThanksComponent } from './thanks/thanks.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'itemdetails', component: ItemdetailsComponent},
+  { path: 'productlist', component: ProductlistComponent },
+  { path: 'thanks', component: ThanksComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
